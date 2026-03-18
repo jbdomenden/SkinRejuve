840526980e76a9cd1fc73493173e9a12aec6d731
Bryce <john.domenden@umtc.com.ph>
2026-03-18 15:58:01 +0800

    Delete src/main/kotlin/Serialization.kt

--- src/main/kotlin/zeroday/skinrejuve/plugins/Routing.kt
+++ src/main/kotlin/zeroday/skinrejuve/plugins/Routing.kt
@@ -1,39 +1,16 @@
 package zeroday.skinrejuve.plugins
 
-import zeroday.skinrejuve.auth.AuthRepository
-import zeroday.skinrejuve.auth.AuthService
-import zeroday.skinrejuve.auth.EmailVerificationService
-import zeroday.skinrejuve.auth.JwtService
-import zeroday.skinrejuve.auth.PasswordResetService
-import zeroday.skinrejuve.auth.authRoutes
-import zeroday.skinrejuve.routes.adminRoutes
-import zeroday.skinrejuve.routes.analyticsRoutes
-import zeroday.skinrejuve.routes.appointmentRoutes
-import zeroday.skinrejuve.routes.notificationRoutes
-import zeroday.skinrejuve.routes.patientRoutes
-import zeroday.skinrejuve.routes.serviceRoutes
-import zeroday.skinrejuve.routes.staffRoutes
-import zeroday.skinrejuve.routes.treatmentRoutes
+import io.ktor.server.application.*
+import io.ktor.server.http.content.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import zeroday.skinrejuve.auth.*
+import zeroday.skinrejuve.routes.*
 import zeroday.skinrejuve.security.JwtConfig
-import zeroday.skinrejuve.services.AnalyticsService
-import zeroday.skinrejuve.services.AppointmentService
-import zeroday.skinrejuve.services.MailService
-import zeroday.skinrejuve.services.NotificationService
-import zeroday.skinrejuve.services.PatientService
-import zeroday.skinrejuve.services.ServiceService
-import zeroday.skinrejuve.services.StaffService
-import zeroday.skinrejuve.services.TreatmentService
+import zeroday.skinrejuve.services.*
 import zeroday.skinrejuve.utils.ApiResponse
 import zeroday.skinrejuve.utils.appConfig
 import zeroday.skinrejuve.utils.mailConfig
-import io.ktor.server.application.Application
-import io.ktor.server.application.call
-import io.ktor.server.response.respond
-import io.ktor.server.response.respondRedirect
-import io.ktor.server.http.content.resources
-import io.ktor.server.http.content.staticResources
-import io.ktor.server.routing.get
-import io.ktor.server.routing.routing
 
 fun Application.configureRouting() {
     val authRepository = AuthRepository()
